513e8cd5de16d9b7a35b4cb400f2bd5f8f7311e9
Anish Karthik <anishkarthika21cs@psnacet.edu.in>
2024-04-28 16:39:02 +0530

    added some scoreboard

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,29 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  // reactStrictMode: true,
+  // webpack5: true,
+  images: {
+    domains: ["res.cloudinary.com", "cdn-icons-png.flaticon.com"],
+    remotePatterns: [
+      {
+        hostname: "res.cloudinary.com",
+        protocol: "https",
+      },
+      {
+        hostname: "cdn-icons-png.flaticon.com",
+        protocol: "https",
+      },
+    ],
+  },
+  // async redirects() {
+  //   return [
+  //     {
+  //       source: '/about',
+  //       destination: '/',
+  //       permanent: true,
+  //     },
+  //   ];
+  // },
+};
 
 export default nextConfig;
---
+++ src/app/(protected)/test/scoreboard/page.tsx
@@ -0,0 +1,8 @@
+import Leaderboard from "@/components/scoreboard";
+import React from "react";
+
+const page = () => {
+  return <Leaderboard />;
+};
+
+export default page;
